e558346fdda3696430e2cf58ab758c68efafd9f0
sidra <sidrajagrawal@gmail.com>
2025-11-21 16:58:35 +0530

    kk

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-// app.use(cors)
+// enable CORS (call the factory to get middleware)
+// app.use(cors());
 
 app.use(cors({
   origin: ['https://manthan-cpnytgv11-sidrajagrawals-projects.vercel.app/' ],
--- frontend/src/components/CallApi.jsx
+++ frontend/src/components/CallApi.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const BASE_API = 'https://manthan-cpnytgv11-sidrajagrawals-projects.vercel.app/api';
-
+const BASE_API = 'https://manthan-cpnytgv11-sidrajagrawals-projects.vercel.app/api/output';
+// const BASE_API = 'http://localhost:8080/api/output';
 
 export const Unchangeable = async (imageUrl, imageName) => {
     try {
         const payload = { imageUrl };
         if (imageName) payload.imageName = imageName;
-
+        console.log("imageName");
+        
         const res = await axios.post(`${BASE_API}/unchangeable`, payload);
         console.log(res);
         
